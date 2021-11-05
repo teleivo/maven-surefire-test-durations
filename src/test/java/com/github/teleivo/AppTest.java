0724f4fbe2cb5264752a81b54fe1baca19c3a3f7
teleivo <teleivo@users.noreply.github.com>
2021-11-05 13:05:48 +0100

    add a BeforeClass to see its effects

--- src/test/java/com/github/teleivo/AppTest.java
+++ src/test/java/com/github/teleivo/AppTest.java
@@ -3,6 +3,7 @@ package com.github.teleivo;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -11,13 +12,24 @@ import org.junit.Test;
 public class AppTest 
 {
 
+    @BeforeClass
+    public static void setUpOnce() throws InterruptedException {
+        Thread.sleep(1000 * 7);
+    }
+
     @Before
     public void setUp() throws InterruptedException {
-        Thread.sleep(1000 * 30);
+        Thread.sleep(1000 * 10);
     }
 
     @Test
     public void shouldAnswerWithTrue() throws InterruptedException {
+        Thread.sleep(1000 * 2);
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithTrueAsWell() throws InterruptedException {
         Thread.sleep(1000 * 2);
         assertTrue( true );
     }
